0b9e2f73e20fec6a03b3509fb0513a6998504c81
tyruzlim1 <107935979+tyruzlim1@users.noreply.github.com>
2022-06-22 23:21:43 +0800

    Add files via upload

---
+++ brightnesscontrol-firefox/content_script.js
@@ -0,0 +1,14 @@
+let style = document.createElement('style');
+document.body.appendChild(style);
+
+browser.storage.onChanged.addListener((changes, area) => {
+    if (area === 'local' && 'value' in changes) {
+        update(changes.value.newValue);
+    }
+});
+
+function update(value) {
+    style.innerText = `html { filter: brightness(${value}%) !important}`;
+}
+
+browser.storage.local.get('value').then(result => update(result.value));
